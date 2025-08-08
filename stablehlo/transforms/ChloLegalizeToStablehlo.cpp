e4f2b22718e0ab2a97ac6c571a9f06c9bfce3da7
William Moses <gh@wsmoses.com>
2025-08-07 21:02:20 -0500

    Update ChloLegalizeToStablehlo.cpp

--- stablehlo/transforms/ChloLegalizeToStablehlo.cpp
+++ stablehlo/transforms/ChloLegalizeToStablehlo.cpp
@@ -4,6 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#define _USE_MATH_DEFINES
 // Implements logic for lowering CHLO ops to StableHLO and Shape dialect ops,
 // taking care of CHLO's broadcasting semantics
 #include <algorithm>
